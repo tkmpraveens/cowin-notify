a56a17a18e8ddc6569963d1283a9351bf7ccbf4c
Praveen <tkmpraveens@gmail.com>
2023-02-14 01:55:26 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-
-import { Landing } from "./Components/Landing";
-
-import "./Styles/main.scss";
-
-ReactDOM.render(<Landing />, document.getElementById("root"));
