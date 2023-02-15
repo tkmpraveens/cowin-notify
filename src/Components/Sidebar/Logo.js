3efb9aa45d70bec6a2fd84608cf84f77dcf3678c
Praveen <tkmpraveens@gmail.com>
2023-02-16 01:14:15 +0530

    Updates

--- src/Components/Sidebar/Logo.js
+++
@@ -1,8 +0,0 @@
-export const Logo = () => {
-  return (
-    <div className="logo">
-      <div className="logo__bold">COWIN</div>
-      <div className="logo__thin">NOTIFY</div>
-    </div>
-  );
-};
